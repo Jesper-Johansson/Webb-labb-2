9eb72aacd9593c9ef9efadb6a739a6b69446d0d1
Jesper <jvj.github@gmail.com>
2023-11-09 17:49:51 +0100

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [
+    HomeComponent,
+    RouterModule
+  ],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'homes';
+}
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
